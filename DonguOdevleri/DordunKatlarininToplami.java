fc87cf7397e30f535da24aec08a0a3392b0f2ccb
fatiOzturk <114157962+fatiOzturk@users.noreply.github.com>
2022-10-16 13:06:45 +0300

    Add files via upload

---
+++ DonguOdevleri/DordunKatlarininToplami.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class DordunKatlarininToplami {
+    public static void main(String[]args){
+        int i,total=0;
+        Scanner input=new Scanner(System.in);
+        do {
+            System.out.print("Sayı giriniz : ");
+            i= input.nextInt();
+            if(i%4==0) {
+                total+=i;
+            }
+        }while (i%2==0);
+        System.out.println("Girilen çift ve dördün katları olan sayıların toplamı: "+total);
+    }
+
+}
