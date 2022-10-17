d35891f04a64a9e2fed4b021e51869ff131b2993
fatiOzturk <114157962+fatiOzturk@users.noreply.github.com>
2022-10-17 22:55:34 +0300

    Add files via upload

---
+++ DonguOdevleri/TersUcgen.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class TersUcgen {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Basamak sayısı giriniz : ");
+        int n = input.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = i; k <= (2 * n) - i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
